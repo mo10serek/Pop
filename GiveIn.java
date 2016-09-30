f08c237a7750ef37688301606de611366d842aad
mo10serek <michael.balcerzak@ocsbstudent.ca>
2016-09-30 13:29:00 -0400

    Update GiveIn.java

--- GiveIn.java
+++ GiveIn.java
@@ -2,7 +2,7 @@
 * Created by: Michael balcerzak
 * Created on: 30-Sep-2016
 * Created for: ICS3U
-* Daily Assignment – Unit#2-02
+* Daily Assignment â€“ Unit#2-02
 * This program makes the user to put something in the stack and take something out
 */
 
@@ -21,10 +21,14 @@ public class GiveIn {
 		//have
 		MrCoxallStack aSingleStack = new MrCoxallStack();
 		aSingleStack.push(have);
+		aSingleStack.push(67.0);
 		
 		//get
 		Double get = aSingleStack.pop();
 		System.out.println(get);
+		
+		Double get2 = aSingleStack.pop();
+		System.out.println(get2);
 		
 	}
 
